8b4485d8c4f4d6996703455e45788470e7905769
sebboli029 <115046212+sebboli029@users.noreply.github.com>
2025-02-17 11:39:35 -0500

    Añadir codigo

---
+++ datos.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from tabulate import tabulate
+
+# Cargar el archivo CSV
+file_path = 'C:/Users/sebbo/Downloads/players.csv'
+df = pd.read_csv(file_path)
+
+# Convertir la columna 'height' a centímetros
+def height_to_cm(height):
+    feet, inches = height.split('-')
+    total_inches = int(feet) * 12 + int(inches)
+    return total_inches * 2.54
+
+df['height'] = df['height'].apply(height_to_cm)
+
+# Calcular el IMC
+df['IMC'] = df['weight'] / (df['height'] / 100) ** 2
+
+# Seleccionar las columnas deseadas y renombrarlas a español
+selected_columns = df[['position', 'height', 'weight', 'IMC']]
+selected_columns.columns = ['posición', 'altura (cm)', 'peso (kg)', 'IMC']
+
+# Calcular el promedio de altura, peso e IMC por cada posición
+average_columns = selected_columns.groupby('posición').mean().reset_index()
+
+# Mostrar los datos promediados usando tabulate con líneas continuas
+print(tabulate(average_columns, headers='keys', tablefmt='fancy_grid'))
